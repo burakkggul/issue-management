947725a64a2418a624db114d0f85dbe3db35d2da
Burak GÜL <burakkggul@gmail.com>
2019-10-10 17:31:18 +0300

    Update Project Repository.

--- src/main/java/com/basarsoft/issuemanagement/repository/ProjectRepository.java
+++ src/main/java/com/basarsoft/issuemanagement/repository/ProjectRepository.java
@@ -10,8 +10,9 @@ import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-    List<Project> getByProjectCode(String projectCode);
+    Project getByProjectCode(String projectCode);
     List<Project> getByProjectCodeContains(String projectCode);
+    Project getByProjectCodeAndIdNot(String projectCode, Long id);
     Page<Project> findAll (Pageable pageable);
     List<Project> findAll (Sort sort);
 }
