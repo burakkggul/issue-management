8c5b676110c61db6b8fa42690332f8b9b79f25c2
Burak GÜL <burakkggul@gmail.com>
2019-10-10 17:26:49 +0300

    Api is created.

--- src/main/java/com/basarsoft/issuemanagement/controller/ProjectController.java
+++ src/main/java/com/basarsoft/issuemanagement/controller/ProjectController.java
@@ -1,24 +1,52 @@
 package com.basarsoft.issuemanagement.controller;
 
+
 import com.basarsoft.issuemanagement.dto.ProjectDto;
 import com.basarsoft.issuemanagement.service.ProjectServiceImpl;
+import com.basarsoft.issuemanagement.util.ApiPaths;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("/project")
+@RequestMapping(ApiPaths.ProjectCtrl.CTRL)
+@Api(value = ApiPaths.ProjectCtrl.CTRL)
 public class ProjectController {
 
     private final ProjectServiceImpl projectService;
 
-    public ProjectController(ProjectServiceImpl projectService){
+    public ProjectController(ProjectServiceImpl projectService) {
         this.projectService = projectService;
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Get By Id Operation.", response = ProjectDto.class)
     public ResponseEntity<ProjectDto> getById(@PathVariable("id") Long id) {
         ProjectDto projectDto = projectService.getById(id);
         return ResponseEntity.ok(projectDto);
+    }
+
+    @PostMapping
+    @ApiOperation(value = "Create Operation.", response = ProjectDto.class)
+    public ResponseEntity<ProjectDto> createProject(@Valid @RequestBody ProjectDto projectDto) {
+        return ResponseEntity.ok(projectService.save(projectDto));
+    }
+
+    //@RequestMapping(path = "update",method = RequestMethod.PUT)
+    @PutMapping("/{id}")
+    @ApiOperation(value = "Update Operation.", response = ProjectDto.class)
+    public ResponseEntity<ProjectDto> updateProject(@PathVariable("id") Long id, @Valid @RequestBody ProjectDto project) {
+        return ResponseEntity.ok(projectService.update(id, project));
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete Operation.", response = Boolean.class)
+    public ResponseEntity<Boolean> delete(@PathVariable(value = "id", required = true) Long id) {
+        return ResponseEntity.ok(projectService.delete(id));
+
     }
 
 }
