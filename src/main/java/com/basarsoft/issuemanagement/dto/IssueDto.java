7c9db7260bc8a2cf2ada2b02caf00fd28a882894
Burak GÜL <burakkggul@gmail.com>
2019-10-10 17:30:01 +0300

    Update Data Transfer Object.

--- src/main/java/com/basarsoft/issuemanagement/dto/IssueDto.java
+++ src/main/java/com/basarsoft/issuemanagement/dto/IssueDto.java
@@ -1,6 +1,8 @@
 package com.basarsoft.issuemanagement.dto;
 
 import com.basarsoft.issuemanagement.model.IssueStatus;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import java.util.Date;
@@ -10,13 +12,21 @@ import java.util.Date;
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
+@ApiModel(value = "Issue Data Transfer Object")
 public class IssueDto {
-    private long id;
+    @ApiModelProperty(value = "ID")
+    private Long id;
+    @ApiModelProperty(required = true, value = "Issue Description.")
     private String description;
+    @ApiModelProperty(required = true, value = "Issue Details.")
     private String details;
+    @ApiModelProperty(required = true, value = "Issue Date.")
     private Date date;
+    @ApiModelProperty(required = true, value = "Issue Status.")
     private IssueStatus issueStatus;
+    @ApiModelProperty(value = "Issue Assignee.")
     private UserDto assignee;
+    @ApiModelProperty(value = "Project of Issue.")
     private ProjectDto project;
 
 }
--- src/main/java/com/basarsoft/issuemanagement/dto/ProjectDto.java
+++ src/main/java/com/basarsoft/issuemanagement/dto/ProjectDto.java
@@ -1,14 +1,27 @@
 package com.basarsoft.issuemanagement.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
+@ApiModel(value = "Project Data Transfer Object")
 public class ProjectDto {
+    @ApiModelProperty(value = "Project ID ")
     private Long id;
+    @NotNull
+    @NotEmpty
+    @ApiModelProperty(required = true, value = "Name Of Project.")
     private String projectName;
+    @NotNull
+    @NotEmpty
+    @ApiModelProperty(required = true, value = "Code Of Project.")
     private String projectCode;
 }
